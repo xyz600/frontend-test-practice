a1b6ec8cecaea71edd76f14c3f71810b73d93d06
xyz600 <xyz600600@gmail.com>
2023-07-29 15:26:03 +0900

    add sub function

--- libs/math/index.test.ts
+++ libs/math/index.test.ts
@@ -1,7 +1,18 @@
-import { add } from "./index"
+import { add, sub } from "./index"
 
 describe("add", () => {
-    test("add: 1 + 2 = 3 になる", () => {
+    test("1 + 2 = 3 になる", () => {
         expect(add(1, 2)).toBe(3);
     });
 });
+
+describe("sub", () => {
+    test("3 - 2 = 1 になる", () => {
+        expect(sub(3, 2)).toBe(1);
+    });
+
+    test("2 - 3 = 0 になる", () => {
+        expect(sub(2, 3)).toBe(0);
+    });
+
+})
--- libs/math/index.ts
+++ libs/math/index.ts
@@ -1,4 +1,11 @@
 
-export function add(a: number, b: number): number {
+export const add = (a: number, b: number): number => {
     return a + b;
+}
+
+export const sub = (a: number, b: number): number => {
+    if (a < b) {
+        return 0;
+    }
+    return a - b;
 }
